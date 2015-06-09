7d8d761a2f7df99b16f1615bc56ddfc33b9538f6
Allan Benamer <allan@socialmediatoday.com>
2015-06-08 22:46:24 -0400

    Call a Drupal URL and provide the correct headers so Drupal doesn't throw an error

---
+++ grupal.go
@@ -0,0 +1,25 @@
+package main // grupal
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://dev-d8go.pantheon.io/d8go/node/1", nil)
+	req.Header.Set("Accept:", "*/*")
+	req.Header.Set("Accept-Encoding:", "gzip, deflate, sdch")
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
+}
