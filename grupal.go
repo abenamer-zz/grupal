039419a4739d10c6206acfb6038e30dcdd8aa33d
Allan Benamer <allan@socialmediatoday.com>
2015-07-01 16:32:05 -0400

    Added query string. This closes #1

--- grupal.go
+++ grupal.go
@@ -1,24 +1,33 @@
-package main // grupal
+// Main contains a Web server that serves as a proxy for a Drupal web site
+package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(rw http.ResponseWriter, req *http.Request) {
+
+	var url string
+	url = "http://dev-d8go.pantheon.io/d8go/node/" + req.URL.Path[1:] + "?_format=json"
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "http://dev-d8go.pantheon.io/d8go/node/1", nil)
-	req.Header.Set("Accept-Encoding:", "gzip, deflate, sdch")
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, sdch")
 	res, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	robots, err := ioutil.ReadAll(res.Body)
+	body_text, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", robots)
+	rw.Write(body_text)
 }
